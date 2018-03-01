a32dde03a7583893f47ea2eeb37a323cb954f394
Чумаков Александр Александрович <achumakov@technoserv.ts>
2018-03-01 20:53:05 +0300

    Add server list main form

--- backend.js
+++ backend.js
@@ -5,29 +5,29 @@ const {ipcMain, app, BrowserWindow} = require('electron');
   
   // Храните глобальную ссылку на объект окна, если вы этого не сделаете, окно будет
   // автоматически закрываться, когда объект JavaScript собирает мусор.
-  let win
+  let win;
   
   function createWindow () {
     // Создаёт окно браузера.
-    win = new BrowserWindow({width: 800, height: 600})
+    win = new BrowserWindow({width: 800, height: 600});
   
     // и загрузит index.html приложение.
     win.loadURL(url.format({
       pathname: path.join(__dirname, 'ui/index.html'),
       protocol: 'file:',
       slashes: true
-    }))
+    }));
   
     // Откроет DevTools.
-    win.webContents.openDevTools()
+    win.webContents.openDevTools();
   
     // Возникает, когда окно будет закрыто.
     win.on('closed', () => {
       // Разбирает объект окна, обычно вы можете хранить окна     
       // в массиве, если ваше приложение поддерживает несколько окон в это время,
       // тогда вы должны удалить соответствующий элемент.
-      win = null
-    })
+      win = null;
+    });
 
     console.log("ready");
   }
@@ -35,24 +35,24 @@ const {ipcMain, app, BrowserWindow} = require('electron');
   // Этот метод будет вызываться, когда Electron закончит 
   // инициализацию и готова к созданию окон браузера.
   // Некоторые интерфейсы API могут использоваться только после возникновения этого события.
-  app.on('ready', createWindow)
+  app.on('ready', createWindow);
   
   // Выйти, когда все окна будут закрыты.
   app.on('window-all-closed', () => {
     // На macOS это обычно для приложений и их строки меню   
     // оставаться активным до тех пор, пока пользователь не выйдет явно с помощью Cmd + Q
     if (process.platform !== 'darwin') {
-      app.quit()
+      app.quit();
     }
-  })
+  });
   
   app.on('activate', () => {
      // На MacOS это общее для того чтобы создать окно в приложении, когда значок 
      // dock нажали и нет других открытых окон.
     if (win === null) {
-      createWindow()
+      createWindow();
     }
-  })
+  });
   
   // В этом файле вы можете включить код другого основного процесса 
   // вашего приложения. Можно также поместить их в отдельные файлы и применить к ним require.
