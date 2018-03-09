c46953fe0e6030d5417ef034ca2838b30ee40d0d
Delfer <ts.delfer@gmail.com>
2018-03-09 17:31:13 +0300

    storage: aes without gzip

--- storage.js
+++ storage.js
@@ -1,14 +1,21 @@
 var crypto = require('crypto'),
   algorithm = 'aes-256-ctr',
-  password = 'gUZa3TASta&e';
+  password = '7ri+!l3+OEf@afLaHlEq7eN?ezlenieT';
+
+var toStream = require('string-to-stream'),
+  toString = require('stream-to-string');
+
+var aesjs = require('aes-js');
 
 var fs = require('fs');
 var zlib = require('zlib');
 
 var filename = 'storage.zip';
 
+var iv = new Buffer.alloc(16);
+
 // zip content
-var zip = zlib.createGzip;
+var zip = zlib.createGzip();
 // encrypt content
 var encrypt = crypto.createCipher(algorithm, password);
 // decrypt content
@@ -18,13 +25,27 @@ var unzip = zlib.createGunzip();
 
 
 exports.read = function () {
-  var obj = [];
+  let obj = [];
+  // let file = fs.createReadStream(filename);
+  // let stream = file.pipe(decrypt).pipe(unzip);
+  // toString(stream).then(function (msg) {
+  //   console.log(msg);
+  //   obj = JSON.parse(msg);
+  //   file.close();
+  // }, function(rsn) { console.log(rsn);});
   try {
-    obj = JSON.parse(decrypt(unzip(fs.readFileSync(filename))));
+    let aesCtr = new aesjs.ModeOfOperation.ctr(aesjs.utils.utf8.toBytes(password));
+    let encrypted = new Uint8Array(fs.readFileSync(filename));
+    console.log('read-enc',encrypted);
+    let zipped = aesCtr.decrypt(encrypted);
+    let decryptedText = aesjs.utils.utf8.fromBytes(zipped);
+    console.log('read-json',decryptedText);
+    obj = JSON.parse(decryptedText);
   } catch (e) {
-    console.log (e);
+    console.log(e);
   }
-  return obj;
+
+    return obj;
 };
 
 exports.create = function (server) {
@@ -35,5 +56,14 @@ exports.create = function (server) {
 };
 
 var write = function (servers) {
-  fs.writeFileSync(filename,encrypt(zip(JSON.stringify(servers))));
+  // let file = fs.createWriteStream(filename);
+  // await toStream(JSON.stringify(servers)).pipe(zip).pipe(encrypt).pipe(file).
+  // file.close();
+  let aesCtr = new aesjs.ModeOfOperation.ctr(aesjs.utils.utf8.toBytes(password));
+  let json = JSON.stringify(servers);
+  console.log('wrtie-json',json);
+  let textBytes = aesjs.utils.utf8.toBytes(json);
+  let encrypted  = aesCtr.encrypt(textBytes);
+  console.log('wrtie-enc',encrypted);
+  fs.writeFileSync(filename,encrypted);
 };
