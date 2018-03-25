4623841478a0347ff17fe0410d40312305b432cd
Delfer <ts.delfer@gmail.com>
2018-03-25 12:20:24 +0300

    Improoved sshell.runOnce

--- sshell.js
+++ sshell.js
@@ -1,10 +1,18 @@
 exports.runOnce = function (con, command) {
+    var res = '';
     return new Promise(function (resolve, reject) {
         con.exec(command, function (err, stream) {
-            if (err) throw err;
-            stream.on('data', function (data) {
-                logCmd(con, command,data);
-                resolve(data);
+            if (err) {
+                logCmd(con, command,undefined,err);
+                reject(err);
+                return;
+            }
+            stream.on('close', function () {
+                logCmd(con, command,res);
+                resolve(res);
+            })
+            .on('data', function (data) {
+                res += data;
             }).stderr.on('data', function (data) {
                 logCmd(con, command,undefined,data);
                 reject(data);
@@ -14,5 +22,5 @@ exports.runOnce = function (con, command) {
 };
 
 var logCmd = function (conn, command, out, err) {
-    log.debug ('Con: %s; com: %s; out: %s; err: %s', CircularJSON.stringify(conn).replace(/"password":".*"/g,'"password":"XXX"'), command, out, err);
+    log.debug('Con: %s; com: %s; out: %s; err: %s', CircularJSON.stringify(conn).replace(/"password":".*"/g, '"password":"XXX"'), command, out, err);
 };
