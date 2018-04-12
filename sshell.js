846adde23c12cca7f5a4a9c42af02286beaa573e
Delfer <ts.delfer@gmail.com>
2018-04-12 18:39:48 +0300

    Added runBashScriptAsRoot function

--- sshell.js
+++ sshell.js
@@ -20,9 +20,9 @@ exports.runCmdAsRootMethod = function (con, command, method, forceBreak) {
         case PEM.JUST_ROOT:
             return exports.runCmd(con, command);
         case PEM.SUDO_NO_PASSWD:
-            return exports.runCmd(con, 'sudo -- sh -c \'' + command.replace(/'/g,'\'') + '\'');
+            return exports.runCmd(con, 'sudo -- sh -c \'' + command.replace(/'/g, '\'') + '\'');
         case PEM.SUDO_PASSWD:
-            return exports.runCmdInteractive(con, 'sudo -- sh -c \'' + command.replace(/'/g,'\'') + '\'', [{
+            return exports.runCmdInteractive(con, 'sudo -- sh -c \'' + command.replace(/'/g, '\'') + '\'', [{
                     regex: /:\s*$/,
                     answer: con.config.password + '\n'
                 },
@@ -32,7 +32,7 @@ exports.runCmdAsRootMethod = function (con, command, method, forceBreak) {
                 }
             ]);
         case PEM.SU:
-            return exports.runCmdInteractive(con, 'su -c  \'' + command.replace(/'/g,'\'') + '\'', [{
+            return exports.runCmdInteractive(con, 'su -c  \'' + command.replace(/'/g, '\'') + '\'', [{
                     regex: /:\s*$/,
                     answer: con.config.rootPassword + '\n'
                 },
@@ -47,12 +47,22 @@ exports.runCmdAsRootMethod = function (con, command, method, forceBreak) {
 };
 
 exports.runCmdAsRoot = function (con, command) {
-    return new Promise(function (resolve, reject) {
-        detectPrivilegeEscalationMethod(con).then(function (method) {
-            exports.runCmdAsRootMethod(con, command, method).then(resolve, reject);
-        }, reject);
+    return detectPrivilegeEscalationMethod(con).then((method) => {
+        exports.runCmdAsRootMethod(con, command, method);
     });
 };
+
+exports.runBashScriptAsRoot = function (con, script, args) {
+    var script64 = Buffer.from(script, 'binary').toString('base64');
+
+    var command = 'echo ' + script64 + ' | base64 -d | /bin/bash /dev/stdin ' + args;
+
+    return rushPackage(con, 'bash')
+        .then(() => rushPackage(con, 'base64'))
+        .then(() => detectPrivilegeEscalationMethod(con))
+        .then((method) => exports.runCmdAsRootMethod(con, command, method));
+};
+
 
 exports.runCmdInteractive = function (con, command, reactions) {
     var stdout = '';
@@ -67,9 +77,9 @@ exports.runCmdInteractive = function (con, command, reactions) {
                 reject(err);
                 return;
             }
-            stream.on('close', function () {
+            stream.on('close', function (code) {
                     logCmd(con, command, stdout, stderr);
-                    if (stderr.length === 0) {
+                    if (code === 0) {
                         resolve(stdout);
                     } else {
                         reject(stderr);
@@ -100,57 +110,70 @@ var detectPrivilegeEscalationMethod = function (con) {
         //Already root
         var checkJustRoot = function () {
             exports.runCmdAsRootMethod(con, 'whoami', PEM.JUST_ROOT)
-            .then(function (data) {
-                if (/\s*root\s*/.test(data)) {
-                    con.privilegeEscalationMethod = PEM.JUST_ROOT;
-                    resolve(con.privilegeEscalationMethod);
-                } else {
-                    checkSudoNoPasswd();
-                }
-            }, checkSudoNoPasswd);
+                .then(function (data) {
+                    if (/\s*root\s*/.test(data)) {
+                        con.privilegeEscalationMethod = PEM.JUST_ROOT;
+                        resolve(con.privilegeEscalationMethod);
+                    } else {
+                        checkSudoNoPasswd();
+                    }
+                }, checkSudoNoPasswd);
         };
 
         //Sudo without password
         var checkSudoNoPasswd = function () {
             exports.runCmdAsRootMethod(con, 'whoami', PEM.SUDO_NO_PASSWD)
-            .then(function (data) {
-                if (/\s*root\s*/.test(data)) {
-                    con.privilegeEscalationMethod = PEM.SUDO_NO_PASSWD;
-                    resolve(con.privilegeEscalationMethod);
-                } else {
-                    checkSudoPasswd();
-                }
-            }, checkSudoPasswd);
+                .then(function (data) {
+                    if (/\s*root\s*/.test(data)) {
+                        con.privilegeEscalationMethod = PEM.SUDO_NO_PASSWD;
+                        resolve(con.privilegeEscalationMethod);
+                    } else {
+                        checkSudoPasswd();
+                    }
+                }, checkSudoPasswd);
         };
 
         //Sudo with password
         var checkSudoPasswd = function () {
             exports.runCmdAsRootMethod(con, 'whoami', PEM.SUDO_PASSWD, true)
-            .then(function (data) {
-                if (/\s*root\s*/.test(data)) {
-                    con.privilegeEscalationMethod = PEM.SUDO_PASSWD;
-                    resolve(con.privilegeEscalationMethod);
-                } else {
-                    checkSu();
-                }
-            }, checkSu);
+                .then(function (data) {
+                    if (/\s*root\s*/.test(data)) {
+                        con.privilegeEscalationMethod = PEM.SUDO_PASSWD;
+                        resolve(con.privilegeEscalationMethod);
+                    } else {
+                        checkSu();
+                    }
+                }, checkSu);
         };
 
         //Su
         var checkSu = function () {
             exports.runCmdAsRootMethod(con, 'whoami', PEM.SU, true)
-            .then(function (data) {
-                if (/\s*root\s*/.test(data)) {
-                    con.privilegeEscalationMethod = PEM.SU;
-                    resolve(con.privilegeEscalationMethod);
-                } else {
-                    reject();
-                }
-            }, reject);
+                .then(function (data) {
+                    if (/\s*root\s*/.test(data)) {
+                        con.privilegeEscalationMethod = PEM.SU;
+                        resolve(con.privilegeEscalationMethod);
+                    } else {
+                        reject();
+                    }
+                }, reject);
         };
 
         checkJustRoot();
     });
+};
+
+var rushPackage = function (con, package) {
+    var command = 'which ' + package +
+        ' || ( apt-get update && apt-get install -y ' + package + ' )' +
+        ' || yum install -y ' + package +
+        ' || zypper install -y ' + package +
+        ' || urpmi --auto ' + package +
+        ' || pacman --noconfirm -Sy ' + package +
+        ' || upgradepkg --install-new ' + package +
+        ' || apk --no-cache add ' + package;
+
+    return exports.runCmdAsRoot(con, command);
 };
 
 var logCmd = function (con, command, out, err) {
