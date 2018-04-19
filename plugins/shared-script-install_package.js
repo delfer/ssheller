e8ff49bef7670ed0b06ee03607d99cdc78ab1e65
Delfer <ts.delfer@gmail.com>
2018-04-20 01:24:20 +0300

    Bugfix sharedScripts.install_package for yum
    Ignore exit code on repository cache update

--- plugins/shared-script-install_package.js
+++ plugins/shared-script-install_package.js
@@ -6,6 +6,6 @@ exports.plugin = function (list, loader) {};
 sharedScripts.install_package = function (con, package) {
   var script = fs.readFileSync('scripts/pacapt', 'UTF-8');
 
-  return sshell.runBashScriptAsRoot(con, script, '--noconfirm -Sy')
+  return sshell.runBashScriptAsRoot(con, script, '--noconfirm -Sy || true') // true to ignore return code; yum will return 100 if have packages to upgrade
     .then(() => sshell.runBashScriptAsRoot(con, script, '--noconfirm -S ' + package));
 };
