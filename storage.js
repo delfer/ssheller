4056d9bf269913e7a93fd74df76db666626244ce
Delfer <ts.delfer@gmail.com>
2018-04-21 19:09:13 +0300

    Storage moved from app's directory to users's apps directory
    electron.app:
    Mac OS: ~/Library/Application Support/<App Name>
    Windows: C:\Users\<User>\AppData\Local\<App Name>
    Linux: ~/.config/<App Name>

--- storage.js
+++ storage.js
@@ -1,22 +1,25 @@
 var aesjs = require('aes-js');
 
+var electron = require('electron');
 var fs = require('fs');
+var path = require('path');
 var zlib = require('zlib');
 
-const FILENAME = 'storage.dat';
+const userDataPath = (electron.app || electron.remote.app).getPath('userData');
+const FILE_PATH = path.join(userDataPath, 'storage.dat');
 const PASSWORD = aesjs.utils.utf8.toBytes('7ri+!l3+OEf@afLaHlEq7eN?ezlenieT');
 
 exports.read = function () {
   var data = {};
   try {
     let aesCtr = new aesjs.ModeOfOperation.ctr(PASSWORD);
-    let encrypted = new Uint8Array(fs.readFileSync(FILENAME));
+    let encrypted = new Uint8Array(fs.readFileSync(FILE_PATH));
     let zip = aesCtr.decrypt(encrypted);
     let json = zlib.gunzipSync(zip);
     data = JSON.parse(json);
   } catch (e) {
-    if (fs.existsSync(FILENAME)) {
-      fs.renameSync(FILENAME, FILENAME + '_broken_' + Date.now());
+    if (fs.existsSync(FILE_PATH)) {
+      fs.renameSync(FILE_PATH, FILE_PATH + '_broken_' + Date.now());
     }
   }
   return data;
@@ -59,7 +62,7 @@ var write = function (data) {
   let json = JSON.stringify(data);
   let zip = new Uint8Array(zlib.gzipSync(json));
   let encrypted = aesCtr.encrypt(zip);
-  fs.writeFileSync(FILENAME, encrypted);
+  fs.writeFileSync(FILE_PATH, encrypted);
 };
 
 var writeServers = function (servers) {
